ff855a92256a2dfbc77167ead85581b3d7540bd2
NothingToSeeHereFolks019 <daepicawesomeone@gmail.com>
2025-05-19 18:59:33 -0700

    Add files via upload

--- FangBlade.py
+++ FangBlade.py
@@ -22,11 +22,30 @@ def draw_bg():
     screen.fill(BG)
 
 class Ninja(pygame.sprite.Sprite):
-    def __init__(self, x, y, scale, speed):
+    def __init__(self, char_type, x, y, scale, speed):
         pygame.sprite.Sprite.__init__(self)
+        self.alive = True
+        self.char_type = char_type
         self.speed = speed
-        img = pygame.image.load('/Users/abhinava/VisualStudioCode/fangblade_assets/New Piskel.png').convert_alpha()
-        self.img = pygame.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
+        self.direction = 1
+        self.flip = False
+        self.animation_list = []
+        self.frame_index = 0
+        self.action = 0
+        self.update_time = pygame.time.get_ticks()
+        temp_list = []
+        for i in range(18):
+            img = pygame.image.load(f'/Users/abhinava/VisualStudioCode/fangblade_assets/Idle/{i}.png').convert_alpha()
+            img = pygame.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
+            temp_list.append(img)
+        self.animation_list.append(temp_list)
+        temp_list = []
+        for i in range(6):
+            img = pygame.image.load(f'/Users/abhinava/VisualStudioCode/fangblade_assets/Run/{i}.png').convert_alpha()
+            img = pygame.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
+            temp_list.append(img)
+        self.animation_list.append(temp_list)
+        self.img = self.animation_list[self.action][self.frame_index]
         self.rect = self.img.get_rect()
         self.rect.center = (x,y)
 
@@ -38,16 +57,46 @@ class Ninja(pygame.sprite.Sprite):
         #assign movement variables if moving left or right
         if moving_left:
             dx = -self.speed
+            self.flip = True
+            self.direction = -1
         if moving_right:
             dx = self.speed
+            self.flip = False
+            self.direction = 1
 
             #update rectangle position
         self.rect.x += dx
         self.rect.y += dy
-    def draw(self):
-        screen.blit(self.img, self.rect)
 
-player = Ninja(200, 200, 3, 5)
+    def update_animation(self):
+        #update the animation
+        ANIMATION_COOLDOWN = 100
+        #update image depending on current frame
+        self.img = self.animation_list[self.action][self.frame_index]
+        #check if enough time has passed since the last update
+        if pygame.time.get_ticks() - self.update_time > ANIMATION_COOLDOWN: 
+            self.update_time = pygame.time.get_ticks()
+            self.frame_index += 1
+        #if the animation has reached the end, reset to 0
+        if self.frame_index >= len(self.animation_list[self.action]):
+            self.frame_index = 0
+
+
+    def update_action(self, new_action):
+        #check if the new action is different from the previous one
+        if new_action != self.action:
+            self.action = new_action
+            #update the frame index to 0
+            self.frame_index = 0
+            #update the time
+            self.update_time = pygame.time.get_ticks()
+
+
+    def draw(self):
+        screen.blit(pygame.transform.flip(self.img, self.flip, False,), self.rect)
+
+player = Ninja('player', 200, 200, 3, 5)
+enemy = Ninja('enemy', 400, 200, 3, 5)
 
 run = True
 while run:
@@ -56,10 +105,19 @@ while run:
 
     draw_bg()
 
+    player.update_animation()
     player.draw()
+    enemy.draw()
 
     player.move(moving_left, moving_right)
 
+    #update player actions
+    if player.alive:
+        if moving_left or moving_right:
+            player.update_action(1)#1: run
+        else:
+            player.update_action(0)#0: idle
+    
     for event in pygame.event.get():
         #quit game
         if event.type == pygame.QUIT:
